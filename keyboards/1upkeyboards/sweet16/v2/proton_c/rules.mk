f151a0ebf1734fc25b1790f18680198b18f52567
rasaf <rasaf@saftop.de>
2020-05-10 19:58:07 +0200

    typo

--- keyboards/1upkeyboards/sweet16/v2/proton_c/rules.mk
+++ keyboards/1upkeyboards/sweet16/v2/proton_c/rules.mk
@@ -2,7 +2,7 @@
 MCU = STM32F303
 
 # vvv --------------------
-# "overwrite" defaults from mcu_selection.mk
+# "overwrite" defaults in mcu_selection.mk
 MCU_LDSCRIPT = STM32F303xE
 BOARD = ST_NUCLEO64_F303RE
 # ^^^ --------------------
