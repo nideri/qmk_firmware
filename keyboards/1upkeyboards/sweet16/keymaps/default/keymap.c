2c4cf2d0edbc934adc591de5955781aec4a71b61
rasaf <rasaf@saftop.de>
2020-05-30 15:16:12 +0200

    cosmetics only

--- keyboards/1upkeyboards/sweet16/keymaps/default/keymap.c
+++ keyboards/1upkeyboards/sweet16/keymaps/default/keymap.c
@@ -10,17 +10,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	RGB_TOG, RGB_HUI, RGB_MOD, KC_CALCULATOR,  
         KC_7,    KC_8,    KC_9,    KC_MS_UP,   
         KC_4,    KC_5,    KC_6,    KC_LOCKING_NUM, 
-        KC_1,    KC_2,    KC_3,    KC_MS_DOWN 
+        KC_1,    KC_2,    KC_3,    KC_MS_DOWN
     )
 #else
     /* KC_SYSTEM_WAKE @ TOP LEFT OF NUMLOCK (X|)*/
     LAYOUT_ortho_4x4(
-		     KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,          KC_PSCREEN, KC_PAUSE, KC_END, KC_HELP,
-		     KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_BSPACE, RGB_TOG, KC_LOCKING_NUM, KC_LOCKING_SCROLL, KC_KP_SLASH,
-/* FUNCTION KEY -> */KC_NO, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRACKET, KC_RBRACKET, KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_ASTERISK,
-		     KC_LCTRL, KC_CAPSLOCK, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE, KC_BSLASH, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_MINUS,
-		     KC_LSHIFT, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_RSHIFT, KC_ENTER, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_PLUS,
-		                       KC_LALT,                                     KC_SPACE,                            KC_RALT,                   KC_KP_0,          KC_KP_DOT, KC_KP_ENTER
+		     KC_ESC,    KC_F1,       KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9,    KC_F10, KC_F11,    KC_F12,                   KC_PSCREEN, KC_PAUSE,       KC_END,            KC_HELP,
+		     KC_GRAVE,  KC_1,        KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,     KC_0,   KC_MINUS,  KC_EQUAL,    KC_BSPACE,   RGB_TOG,    KC_LOCKING_NUM, KC_LOCKING_SCROLL, KC_KP_SLASH,
+/* FUNCTION KEY -> */KC_NO,     KC_TAB,      KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,     KC_O,   KC_P,      KC_LBRACKET, KC_RBRACKET, KC_KP_7,    KC_KP_8,        KC_KP_9,           KC_KP_ASTERISK,
+		     KC_LCTRL,  KC_CAPSLOCK, KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,     KC_L,   KC_SCOLON, KC_QUOTE,    KC_BSLASH,   KC_KP_4,    KC_KP_5,        KC_KP_6,           KC_KP_MINUS,
+		     KC_LSHIFT, KC_NO,       KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMMA, KC_DOT, KC_SLASH,  KC_RSHIFT,   KC_ENTER,    KC_KP_1,    KC_KP_2,        KC_KP_3,           KC_KP_PLUS,
+		                              KC_LALT,                     KC_SPACE,                              KC_RALT,                           KC_KP_0,                    KC_KP_DOT,         KC_KP_ENTER
 		     )
 #endif    
 };
