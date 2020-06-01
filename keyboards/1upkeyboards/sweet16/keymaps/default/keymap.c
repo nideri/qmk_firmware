d0ffe9b5a26e72837925f4c9731476df3883cc8f
rasaf <rasaf@saftop.de>
2020-06-01 07:15:15 +0200

    added left windows key, mapped on key next to lshift

--- keyboards/1upkeyboards/sweet16/keymaps/default/keymap.c
+++ keyboards/1upkeyboards/sweet16/keymaps/default/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		     KC_GRAVE,  KC_1,        KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,     KC_0,   KC_MINUS,  KC_EQUAL,    KC_BSPACE,   RGB_TOG,    KC_LOCKING_NUM, KC_LOCKING_SCROLL, KC_KP_SLASH,
 /* FUNCTION KEY -> */KC_NO,     KC_TAB,      KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,     KC_O,   KC_P,      KC_LBRACKET, KC_RBRACKET, RGB_MOD,    RGB_VAI,        RGB_SAI,           KC_KP_ASTERISK,
 		     KC_LCTRL,  KC_CAPSLOCK, KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,     KC_L,   KC_SCOLON, KC_QUOTE,    KC_BSLASH,   RGB_HUD,    RGB_M_P,        RGB_HUI,           KC_KP_MINUS,
-		     KC_LSHIFT, KC_NO,       KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMMA, KC_DOT, KC_SLASH,  KC_RSHIFT,   KC_ENTER,    RGB_RMOD,   RGB_VAD,        RGB_SAD,           MU_TOG,
+		     KC_LSHIFT, KC_LGUI,     KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMMA, KC_DOT, KC_SLASH,  KC_RSHIFT,   KC_ENTER,    RGB_RMOD,   RGB_VAD,        RGB_SAD,           MU_TOG,
 		                              KC_LALT,                     KC_SPACE,                              KC_RALT,                           KC_KP_0,                    KC_KP_DOT,         AU_TOG
 		     )
     #endif
